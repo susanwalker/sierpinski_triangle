1e849d2f25844037253ff5910169d93449adb3dc
Susan Walker <susanwalker8@gmail.com>
2020-12-04 19:32:10 -0500

    Add new elixir project and add triangle struct

---
+++ lib/sierpinski_triangle/triangle.ex
@@ -0,0 +1,3 @@
+defmodule SierpinskiTriangle.Triangle do
+  defstruct [:x1, :y1, :x2, :y2, :x3, :y3]
+end
---
+++ test/sierpinski_triangle_test.exs
@@ -0,0 +1,8 @@
+defmodule SierpinskiTriangleTest do
+  use ExUnit.Case
+  doctest SierpinskiTriangle
+
+  test "greets the world" do
+    assert SierpinskiTriangle.hello() == :world
+  end
+end
